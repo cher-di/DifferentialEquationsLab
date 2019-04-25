30f39c9a5f0a5e1a2b0fbd8f0b20559ce7346813
cher-di <cher-di@mail.ru>
2019-04-25 18:49:56 +0300

    Euler method was implemented

--- Lab.py
+++ Lab.py
@@ -2,21 +2,22 @@ import math
 import numpy as np
 
 
-def solve_cauchy(x0: float, y0: float, dy0: float):
+def solve_cauchy_analytic(x0: float, y0: float, dy0: float):
     """
     Принимает на вход условия для задачи Коши и возврщает функцию, из семейства функций,
     удовлетворяющую условиям задачи Коши
-    Семейство функций имеет вид: y = C1*e^3x + C2*xe^3x + 1/9*x^2 + 1/27*x + 1/3
+    Уравнение: y'' - 6*y' + 9*y = x^2 - x + 3
+    Общее решение: y = C1*e^3x + C2*xe^3x + 1/9*x^2 + 1/27*x + 1/3
 
     :param x0: точка, для которой известны начальные условия задачи Коши
     :param y0: функция в точке x0
     :param dy0: производная функции в точке x0
     :return: функция, удовлетворяющая заданным условиям
     """
-    A1 = [math.exp(3*x0), x0*math.exp(3*x0)]
-    B1 = y0 - (1/9*x0**2 + 1/27*x0 + 1/3)
-    A2 = [3*math.exp(3*x0), (3*x0 + 1)*math.exp(3*x0)]
-    B2 = dy0 - (2/9*x0 + 1/27)
+    A1 = [math.exp(3 * x0), x0 * math.exp(3 * x0)]
+    B1 = y0 - (1 / 9 * x0 ** 2 + 1 / 27 * x0 + 1 / 3)
+    A2 = [3 * math.exp(3 * x0), (3 * x0 + 1) * math.exp(3 * x0)]
+    B2 = dy0 - (2 / 9 * x0 + 1 / 27)
     A = np.array([A1,
                   A2])
     B = np.array([[B1],
@@ -27,16 +28,56 @@ def solve_cauchy(x0: float, y0: float, dy0: float):
     return lambda x: C1 * math.exp(3 * x) + C2 * x * math.exp(3 * x) + 1 / 9 * x ** 2 + 1 / 27 * x + 1 / 3
 
 
-def prepare_data_for_plotting_func(func: callable, X: tuple, step=0.01):
+def prepare_data_for_plotting_func(func: callable, borders: tuple, step=0.01):
     """
     Подготавливает данные для построения графика функции
 
     :param func: сама фукнция
-    :param X: Границы
+    :param borders: границы
     :param step: шаг дискретезации
     :return: (x, y)
     """
-    start, finish = X
+    start, finish = borders
     x = np.arange(start, finish, step)
     y = np.array([func(el) for el in x])
     return x, y
+
+
+def solve_euler(x0: float, y0: float, dy0: float, borders: tuple, h=0.01):
+    """
+    Принимает на вход условия для задачи Коши и границы отрезка, на котором нужно решить ДУ,
+    решает уравнение методом Эйлера с помощью построения сетки на необходимом отрезке
+    и возврщает решение в узлах сетки.
+    Уравнение: y'' - 6*y' + 9*y = x^2 - x + 3
+
+    :param x0: точка, для которой известны начальные условия задачи Коши
+    :param y0: функция в точке x0
+    :param dy0: производная функции в точке x0
+    :param borders: границы
+    :param h: шаг сетки
+    :return: двумерный массив, первая строка - X, вторая - Y
+    """
+    left, right = borders
+    if x0 > left:
+        raise ArithmeticError
+
+    xj, dyj, yj = x0, dy0, y0
+    for xj in np.arange(x0, left + h, h):
+        dyj_next = dyj + h * (6 * dyj - 9 * yj + xj ** 2 - xj + 3)
+        yj_next = yj + h * dyj
+        dyj = dyj_next
+        yj = yj_next
+
+    steps_num = np.int(np.ceil((right - left) / h))
+    shape = (3, steps_num)
+    grid = np.empty(shape, np.float)
+    grid[:, 0] = [xj,
+                  dyj,
+                  yj]
+    grid[0] = np.arange(left + h, right + h, h)
+    for j in np.arange(steps_num - 1):
+        xj, dyj, yj = grid[:, j]
+        grid[1, j + 1] = dyj + h * (6 * dyj - 9 * yj + xj ** 2 - xj + 3)
+        grid[2, j + 1] = yj + h * dyj
+
+    return grid[(0, 2), :]
--- LabScript.py
+++ LabScript.py
@@ -1,16 +1,27 @@
 import Lab
 import matplotlib.pyplot as plt
+import numpy as np
 
-x0 = 5
+x0 = 0
 y0 = 4
-dy0 = 2
+dy0 = 10
 X = (0, 10)
+h = 10 ** -3
 
-func = Lab.solve_cauchy(x0, y0, dy0)
-x, y = Lab.prepare_data_for_plotting_func(func, X)
+func = Lab.solve_cauchy_analytic(x0, y0, dy0)
+xa, ya = Lab.prepare_data_for_plotting_func(func, X, h)
+xc, yc = Lab.solve_euler(x0, y0, dy0, X, h)
+xer, yer = xa, ((ya - yc) / np.abs(ya)) * 100
 
-plt.plot(x, y, color='green', label=u'x0={}; y0={}; dy0={}'.format(x0, y0, dy0))
-plt.title(u'Решение задачи Коши')
+plt.plot(xa, ya, color='green', label=u'Аналитическое решение')
+plt.plot(xc, yc, color='blue', label=u'Численное решение')
+plt.title(u'Метод Эйлера')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+plt.plot(xer, yer, color='red')
+plt.title(u'Невязка')
+plt.grid(True)
+plt.show()
+plt.close()
