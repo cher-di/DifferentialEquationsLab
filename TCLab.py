d7b539dcad799531d532ee27600e09e3d51e75a0
cher-di <cher-di@mail.ru>
2019-05-06 10:47:50 +0500

    Implemented new version of implicit solution, which does mot work

--- TCLab.py
+++ TCLab.py
@@ -30,25 +30,82 @@ def solve_analytically(t, x, n=500):
     return U + v
 
 
+def solve_euler_explicit(x: tuple, t: tuple, hx: float, ht: float):
+    # x0, xmax = x
+    # t0, tmax = t
+    # size_x = np.arange(x0, xmax + hx, hx).size
+    # size_t = np.arange(t0, tmax + ht, ht).size
+    # size_vertical = size_x + size_t - 2
+    # size_horizontal = size_x
+    # u = np.zeros((size_vertical, size_horizontal), dtype=np.float)
+    # u[:, 0] = 2 * np.linspace(t0, t0 + ht * (size_vertical - 1), size_vertical).T
+    # u[:, 1] = 3 * np.linspace(t0, t0 + ht * (size_vertical - 1), size_vertical).T ** 2
+    # u[1, 2:] = ht * (t0 + ht) * np.linspace(x0 + hx * 2, x0 + hx * (size_horizontal - 1), size_horizontal - 2)
+
+    # for i in range(1, size_horizontal - 1):
+    #     for j in range(2, size_vertical - i):
+    #         tj = t0 + ht * (j - 1)
+    #         xi = x0 + hx * (i - 1)
+    #         u[j, i + 1] = 2 * u[j, i] - u[j, i - 1] + hx ** 2 * ((u[j + 1, i] - u[j, i]) / ht - tj * np.sinh(xi))
+
+    # return u[:size_t, :size_x]
+
+    x0, xmax = x
+    t0, tmax = t
+    shape = (int((tmax - t0) / ht) + 1, int((xmax - x0) / hx) + 1)
+    u = np.zeros(shape, dtype=np.float)
+    u[:, 0] = 2 * np.linspace(t0, tmax, shape[0]).T
+    u[:, 1] = 3 * np.linspace(t0, tmax, shape[0]).T ** 2
+
+    for i in range(1, shape[1] - 1):
+        for n in range(0, shape[0] - 1):
+            tn1 = t0 + ht * n
+            xi = x0 + hx * (i - 1)
+            u[n + 1, i + 1] = 2 * u[n + 1, i] - u[n + 1, i - 1] + \
+                hx ** 2 * ((u[n + 1, i] - u[n , i]) / ht - tn1 * np.sinh(xi))
+
+    return u
+
+
 def solve_euler_implicit(x: tuple, t: tuple, hx: float, ht: float):
     x0, xmax = x
     t0, tmax = t
-    size_x = np.arange(x0, xmax + hx, hx).size
-    size_t = np.arange(t0, tmax + ht, ht).size
-    size_vertical = size_x + size_t - 2
-    size_horizontal = size_x
-    u = np.zeros((size_vertical, size_horizontal), dtype=np.float)
-    u[:, 0] = 2 * np.linspace(t0, t0 + ht * (size_vertical - 1), size_vertical).T
-    u[:, 1] = 3 * np.linspace(t0, t0 + ht * (size_vertical - 1), size_vertical).T ** 2
-    u[1, 2:] = ht * (t0 + ht) * np.linspace(x0 + hx * 2, x0 + hx * (size_horizontal - 1), size_horizontal - 2)
+    shape = (int((tmax - t0) / ht) + 1, int((xmax - x0) / hx) + 1)
+    u = np.zeros(shape, dtype=np.float)
+    u[:, 0] = 2 * np.linspace(t0, tmax, shape[0]).T
+    u[:, -1] = 3 * np.linspace(t0, tmax, shape[0]).T ** 2
+    f = lambda n, i: (t0 + ht * (n - 1)) * np.sinh(x0 + hx * (i - 1))
 
-    for i in range(1, size_horizontal - 1):
-        for j in range(2, size_vertical - i):
-            tj = t0 + ht * (j - 1)
-            xi = x0 + hx * (i - 1)
-            u[j, i + 1] = 2 * u[j, i] - u[j, i - 1] + hx ** 2 * ((u[j + 1, i] - u[j, i]) / ht - tj * np.sinh(xi))
+    # for n in range(0, shape[0] - 1):
+        # Ai = np.eye(shape[1], k=-1) / hx ** 2
+        # Ai[-1, -2] = 0
+        # Bi = np.eye(shape[1], k=0) * (2 / hx ** 2 + 1 / ht)
+        # Ci = np.eye(shape[1], k=1) / hx ** 2
+        # Ci[0, 1] = 0
 
-    return u[:size_t, :size_x]
+        # Fi = np.zeros((shape[1], 1))
+        # for i in range(0, shape[1]):
+        #     Fi[i, 0] = u[n, i] / ht + f(n + 1, i)
+
+        # Fi[1, 0] = Fi[1, 0] - Ai[1, 0] * u[n, 0]
+        # Fi[-2, 0] = Fi[-2, 0] - Ci[-2, -1] * u[n, -1]
+
+        # X = np.linalg.solve((Ai - Bi + Ci)[1:-2, 1:-2], Fi[1:-2])
+        # u[n + 1, 1:-2] = X.T
+
+    Ai = ht / hx ** 2
+    Ci = - 2 * ht / hx ** 2 - 1
+    Bi = Ai
+    for n in range(1, shape[0]):
+        ai = bi = 0
+        for i in range(1, shape[1] - 1):
+            Fi = -u[n, i - 1] - ht * f(n, i - 1)
+            ai_new = - Bi / (Ai * ai + Ci)
+            bi_new = (Fi - Ai * bi) / (Ai * ai + Ci)
+            ai, bi = ai_new, bi_new
+            u[n, i] = (u[n, i - 1] - bi) / ai
+
+    return u
 
 
 def linear_approximation(u, t0, x0, ht, hx, t, x):
--- TCLabScript.py
+++ TCLabScript.py
@@ -4,8 +4,8 @@ import TCLab
 
 x = (0, 1)
 t = (0, 0.5)
-hx = 0.01
-ht = 0.01
+hx = 10 ** -2
+ht = 10 ** -2
 
 # n = np.arange(1, 501, 1, dtype=np.int)
 # u = np.array([TCLab.solve_analytically(0.5, 0.5, i) for i in n])
@@ -18,10 +18,27 @@ ht = 0.01
 # plt.grid(True)
 # plt.show()
 
-tt = np.linspace(t[0], t[1], 1000)
-for xcurr in np.linspace(0, 1, 5):
-    u = TCLab.solve_analytically(tt, xcurr)
-    plt.plot(tt, u, label=u'x={}'.format(xcurr))
+# tt = np.linspace(t[0], t[1], 1000)
+# for xcurr in np.linspace(0, 1, 5):
+#     u = TCLab.solve_analytically(tt, xcurr)
+#     plt.plot(tt, u, label=u'x={}'.format(xcurr))
+# plt.grid(True)
+# plt.legend()
+# plt.show()
+
+x_test = 0.4
+tt = np.linspace(t[0], t[1], int((t[1] - t[0]) / ht) + 1)
+u_analytic = TCLab.solve_analytically(tt, x_test)
+u_euler_explicit = TCLab.solve_euler_explicit(x, t, hx, ht)
+u_euler_implicit = TCLab.solve_euler_implicit(x, t, hx, ht)
+
+plt.plot(tt, u_analytic, label=u'Аналитическое решение')
+plt.plot(tt, u_euler_explicit[:, int(np.floor(x_test / hx))], label=u'Явная схема')
+plt.plot(tt, u_euler_implicit[:, int(np.floor(x_test / hx))], label=u'Неявная схема')
+
+plt.xlabel(u't')
+plt.ylabel(u'u')
+plt.title(u'x = {}'.format(x_test))
 plt.grid(True)
 plt.legend()
 plt.show()
