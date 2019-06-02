942a6a65d31444cad43e7d98744f08eedc87b216
cher-di <cher-di@mail.ru>
2019-06-02 18:19:23 +0300

    Added u(0.5, 0.5) graphs

--- TCLabScript.py
+++ TCLabScript.py
@@ -7,13 +7,9 @@ t = (0, 0.5)
 hx = 0.01
 ht = 0.01
 
-# x_test = 0.7
-tt = np.arange(t[0], t[1] + ht, ht)
-u_analytic = np.array([TCLab.solve_analytically(tt, x_test) for x_test in np.arange(x[0], x[1] + hx, hx)]).T
 u_euler_implicit = TCLab.solve_implicit_schema(x, t, hx, ht)
 
 print('Implicit:', TCLab.get_u_xt(u_euler_implicit, x, t, 0.5, 0.5))
-print('Analytic:', TCLab.get_u_xt(u_analytic, x, t, 0.5, 0.5))
 
 plt.imshow(u_euler_implicit)
 plt.colorbar()
@@ -24,12 +20,33 @@ plt.title(u'Уравнение теплопроводности численно
 plt.grid(True)
 plt.show()
 
-plt.imshow(u_analytic)
+step = np.array((0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001))
+u_comparison = np.zeros((step.size, step.size))
+for i, hx in enumerate(step):
+    for j, ht in enumerate(step):
+        u = TCLab.solve_implicit_schema(x, t, hx, ht)
+        u_comparison[i, j] = TCLab.get_u_xt(u, x, t, 0.5, 0.5)
+        print('Completed ({}, {})'.format(hx, ht))
+
+plt.imshow(u_comparison)
 plt.colorbar()
 
-plt.xlabel(u'x')
-plt.ylabel(u't')
-plt.title(u'Уравнение теплопроводности аналитическое решение')
+plt.xlabel(u'hx')
+plt.ylabel(u'ht')
+plt.title(u'Зависимость значения u(0.5, 0.5) от шага сетки')
 plt.grid(True)
 plt.show()
 
+plt.plot(1/step, u_comparison[:, 2], color='green', marker='.')
+plt.xlabel(u'hx')
+plt.ylabel(u'u(0.5, 0.5)')
+plt.title(u'Зависимость значения u(0.5, 0.5) от шага сетки по координате х')
+plt.grid(True)
+plt.show()
+
+plt.plot(1/step, u_comparison[2, :], color='darkblue', marker='.')
+plt.xlabel(u'ht')
+plt.ylabel(u'u(0.5, 0.5)')
+plt.title(u'Зависимость значения u(0.5, 0.5) от шага сетки по координате t')
+plt.grid(True)
+plt.show()
